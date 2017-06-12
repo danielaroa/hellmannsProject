8f7732ff0a9e0b5cf4917542ff5fbd7d0db7fd07
Nicol Ridente <nicol655@gmail.com>
2017-06-11 23:49:00 -0300

    Adding all project files

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-main
+
+$(document).ready(function(){
+	$('.resultado__slider').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		{
+		  breakpoint: 600,
+		  settings: {
+		    slidesToShow: 2,
+		    slidesToScroll: 1
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+		    slidesToShow: 1,
+		    slidesToScroll: 1
+		  }
+		}  
+			]
+		});
+	$('.slick-current').next().find('label').css('visibility','visible');
+  	$('.slick-arrow').on('click',function(){
+  		$('.slick-slide').find('label').css('visibility','hidden');
+  		$('.slick-current').next().find('label').css('visibility','visible');
+  	});
+});
